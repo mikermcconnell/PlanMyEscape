61b7967639a87017c6e364d1735502dc8bcff412
mikermcconnell <80167613+mikermcconnell@users.noreply.github.com>
2025-11-28 09:50:38 -0500

    Fix: Update Web Firebase config with correct credentials

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCC7Z05xhoWleAQCSUSm0RCMU3bfeeXNgE",
+  authDomain: "planmyescape-b38b8.firebaseapp.com",
+  projectId: "planmyescape-b38b8",
+  storageBucket: "planmyescape-b38b8.firebasestorage.app",
+  messagingSenderId: "650087453484",
+  appId: "1:650087453484:web:fb62304a877203b2f6d289",
+  measurementId: "G-0FDEETZ1S3"
 };
 
 const app = initializeApp(firebaseConfig);
